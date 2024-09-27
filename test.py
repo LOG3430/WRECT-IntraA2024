1f075bdd7347b9ac004069a7db069e7e5c0faaa3
Daryl Barampanze <daryl.bar@gmail.com>
2024-09-27 00:22:19 -0400

    Solution added for WRECT testing.

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
 import unittest
 from tire_classification import Type, classify_tire
 
-# Call "python -m unittest" to run your test 
-# Note that python might be python3 or python3.9 or whatever name you chose during installation
+# Equivalent class 
+# traction_index : above 110, below 110 and above 90, below 90 and above 40 or below 40
+# circonference : below 26 and above 26 
+# pressure : below 36 or above 36
+# Satisfy the critieria for WRECT 
+
 class TestFunction(unittest.TestCase):
-    pass
+    def test_case_1(self):
+        self.assertEqual(Type.INVALID, classify_tire(110, 9, 36))
+
+    def test_case_2(self):
+        self.assertEqual(Type.INVALID, classify_tire(100, 37, 25))
+
+    def test_case_3(self):
+        self.assertEqual(Type.SUMMER, classify_tire(89, 37, 36))
+
+    def test_case_4(self):
+        self.assertEqual(Type.INVALID, classify_tire(37, 37, 36))
